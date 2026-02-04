79901e6c186b4429ff85156ebbb260de998960dd
Srinu <srinu18yt@gmail.com>
2026-02-04 15:25:22 +0530

    Completed the basic UI management

---
+++ Assets/Scripts/UI/uiState.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class uiState : MonoBehaviour
+{
+    public enum UIState
+    {
+        startMenu,
+        DiffSelectMenu,
+        InGameUI,
+        SettingsMenu  
+    }
+}
